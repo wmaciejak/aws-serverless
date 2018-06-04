934c2b2f3b82e9d4f91f3812014dafd5ce8c62c4
Wojciech Maciejak <wojtekmaciej@gmail.com>
2018-06-04 23:57:49 +0200

    Add basic REST endpoints

--- handler.js
+++ handler.js
@@ -1,14 +1,34 @@
 'use strict';
 
-var { graphql } = require('graphql');
-var { schema } = require('./graphql/schema');
+const { graphql } = require('graphql');
+const { schema } = require('./graphql/schema');
+const { postsData } = require('./mocks/posts')
+const { getPostsFromPG } = require('./db/api')
 
-module.exports.graphql_query = (event, context, callback) => graphql(schema, event.queryStringParameters.query)
-  .then(
-    result => callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(result)
-    }),
-    err => callback(err)
-  )
+module.exports.graphql = (event, context, callback) =>
+  graphql(schema, event.queryStringParameters.query)
+    .then(
+      result => callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(result)
+      }),
+      err => callback(err)
+    )
 
+module.exports.rest_mocked_posts = (event, context, callback) => {
+  return callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(postsData)
+  });
+}
+
+module.exports.rest_pg_posts = (event, context, callback) => {
+  return getPostsFromPG()
+    .then(
+      result => callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(result)
+      }),
+      err => callback(err)
+    )
+}
